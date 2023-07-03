bfa617fa3385660ac249ececb7a1240f56103b27
brunovgois <brunovgois@gmail.com>
2023-07-02 23:30:16 -0300

    refact component to allow optional children

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,6 +1,7 @@
 import { Minus, Plus, ShoppingCartSimple } from "@phosphor-icons/react";
+import { ReactNode } from "react";
 
-export function AddToCartButton() {
+export function AddToCartButton({ children }: { children?: ReactNode }) {
   function handleAddToCart() {
     console.log("click");
   }
@@ -8,24 +9,28 @@ export function AddToCartButton() {
   function handleAddAmount() {}
 
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 max-h-6 p-0.5">
       <div className="flex items-center gap-2 bg-gray-200 p-2 rounded-md">
         <button onClick={handleAddAmount}>
-          <Minus className="text-purple-500"/>
+          <Minus className="text-purple-500" />
         </button>
 
         <span>1</span>
         <button onClick={handleAddAmount}>
-          <Plus className="text-purple-500"/>
+          <Plus className="text-purple-500" />
         </button>
       </div>
 
-      <button
-        onClick={handleAddToCart}
-        className="bg-purple-700 rounded-md p-2"
-      >
-        <ShoppingCartSimple className="fill-white w-5 h-5" />
-      </button>
+      {children ? (
+        children
+      ) : (
+        <button
+          onClick={handleAddToCart}
+          className="bg-purple-700 rounded-md p-2"
+        >
+          <ShoppingCartSimple className="fill-white w-5 h-5" />
+        </button>
+      )}
     </div>
   );
 }
