968bcbd7d136ba22f29a9a6f571fdf7ef932ef78
brunovgois <brunovgois@gmail.com>
2023-07-03 20:29:17 -0300

    form types, input form layout, payment container

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -1,6 +1,6 @@
-import { MapPinLine, Trash } from "@phosphor-icons/react";
+import { MapPinLine, Trash, CurrencyDollar } from "@phosphor-icons/react";
 import React from "react";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { AddToCartButton } from "../components/AddToCartButton";
 
 const cartMock = [
@@ -23,46 +23,110 @@ const cartMock = [
     amount: 2,
   },
 ];
+type Inputs = {
+  cep: string;
+  rua: string;
+  numero: string;
+  complemento: string;
+  bairro: string;
+  cidade: string;
+  uf: string;
+};
 export function ShoppingCart() {
-  const { register, handleSubmit, reset } = useForm();
+  const { register, handleSubmit, reset } = useForm<Inputs>();
 
-  function handleSendAddress(data: any) {
+  const handleSendAddress: SubmitHandler<Inputs> = (data) => {
     console.log(data);
     reset();
-  }
+  };
 
   return (
     <div className="flex gap-8">
       <div className="">
-        <h2>Complete seu pedido</h2>
-        <div className="bg-gray-100 rounded-md p-10 max-w-xl">
-          <div className="flex">
-            <MapPinLine className="fill-yellow-600" />
-            <div>
-              <h3>Endereço de entrega</h3>
-              <p>Informe o endereço onde deseja receber seu pedido</p>
+        <div className="flex flex-col gap-3">
+          <h2>Complete seu pedido</h2>
+          <div className="bg-gray-100 rounded-md p-10 max-w-xl">
+            <div className="flex gap-3">
+              <MapPinLine className="fill-yellow-600 w-6 h-6" />
+              <div>
+                <h3>Endereço de entrega</h3>
+                <p>Informe o endereço onde deseja receber seu pedido</p>
+              </div>
+            </div>
+            <form
+              onSubmit={handleSubmit(handleSendAddress)}
+              className="flex-grow-0"
+            >
+              <div className="flex flex-col gap-4">
+                <input
+                  type="text"
+                  placeholder="CEP"
+                  {...register("cep")}
+                  className="w-1/3"
+                />
+                <input
+                  type="text"
+                  placeholder="Rua"
+                  {...register("rua")}
+                  className=""
+                />
+
+                <div className="flex gap-3">
+                  <input
+                    type="text"
+                    placeholder="Número"
+                    {...register("numero")}
+                  />
+                  <input
+                    type="text"
+                    placeholder="Complemento"
+                    {...register("complemento")}
+                    className="w-full"
+                  />
+                </div>
+                <div className="flex gap-3">
+                  <input
+                    type="text"
+                    placeholder="Bairro"
+                    {...register("bairro")}
+                    className="w-1/3"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Cidade"
+                    {...register("cidade")}
+                    className="w-full"
+                  />
+                  <input type="text" placeholder="UF" {...register("uf")} className="w-1/6"/>
+                </div>
+              </div>
+              <button type="submit">temp</button>
+            </form>
+          </div>
+          <div className="bg-gray-100 rounded-md p-10 max-w-xl">
+            <div className="flex gap-3 mb-3">
+              <CurrencyDollar className="text-purple-400 w-6 h-6" />
+              <div>
+                <h3>Pagamento</h3>
+                <p>
+                  O pagamento é feito na entrega. Escolha a forma que deseja
+                  pagar
+                </p>
+              </div>
+            </div>
+
+            <div className="flex gap-3">
+              <button className="bg-gray-200 gap-3 flex p-4 items-center rounded-md">
+                Cartão de Crédito
+              </button>
+              <button className="bg-gray-200 gap-3 flex p-4 items-center rounded-md">
+                Cartão de Débito
+              </button>
+              <button className="bg-gray-200 gap-3 flex p-4 items-center rounded-md">
+                Dinheiro
+              </button>
             </div>
           </div>
-          <form
-            action=""
-            onSubmit={handleSubmit(handleSendAddress)}
-            className="flex-grow-0"
-          >
-            <div>
-              <input type="text" placeholder="CEP" {...register("cep")} />
-              <input type="text" placeholder="Rua" {...register("rua")} />
-              <input type="text" placeholder="Número" {...register("numero")} />
-              <input
-                type="text"
-                placeholder="Complemento"
-                {...register("complemento")}
-              />
-              <input type="text" placeholder="Bairro" {...register("bairro")} />
-              <input type="text" placeholder="Cidade" {...register("cidade")} />
-              <input type="text" placeholder="UF" {...register("uf")} />
-            </div>
-            <button type="submit">temp</button>
-          </form>
         </div>
       </div>
 
