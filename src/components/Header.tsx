c101249d92ee572a119f14c67851013ec6e70d89
brunovgois <brunovgois@gmail.com>
2023-07-05 00:13:04 -0300

    use user location for map pin component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,37 @@
 import { NavLink } from "react-router-dom";
 import Logo from "../assets/Logo.svg";
 import { MapPin, ShoppingCart } from "@phosphor-icons/react";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CoffeeCartContext } from "../contexts/CartContext";
+import axios from "axios";
 
 export function Header() {
   const { test } = useContext(CoffeeCartContext);
+  
+  const [neighborhood, setNeighborhood] = useState('Bairro')
+  const [uf, setUf] = useState('UF')
+  
+  useEffect(() => {
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(showPosition as unknown as PositionCallback)
+    }
+
+    async function showPosition(position: { coords: { latitude: string; longitude: string; }; }) {
+      const res = await axios.get(
+        'https://www.mapquestapi.com/geocoding/v1/reverse?key=TPWvRrsfpgGWBACqqXd94fbDcfVpy2WJ&location=' +
+          position.coords.latitude +
+          '%2C' +
+          position.coords.longitude +
+          '&outFormat=json&thumbMaps=false',
+      )
+
+      if (res.status === 200) {
+        setNeighborhood(res.data.results[0].locations[0].adminArea6)
+        setUf(res.data.results[0].locations[0].adminArea3)
+      }
+    }
+  }, [])
+  
   return (
     <nav className="flex items-center justify-between py-8">
       <NavLink to="/">
@@ -16,14 +42,14 @@ export function Header() {
       <div className="flex items-center gap-3">
         <div className="bg-purple-100 flex py-3 px-2 rounded-md gap-1">
           <MapPin size={24} className="fill-purple-800 " />
-          <p className="text-purple-800">Brasília, DF</p>
+          <p className="text-purple-800">{neighborhood}, {uf}</p>
         </div>
 
         <div className="bg-yellow-50 py-2 px-2 rounded-md">
           <NavLink to="/cart">
             <ShoppingCart size={24} className="fill-yellow-800" />
           </NavLink>
-          test
+          {test}
         </div>
       </div>
     </nav>
