e2b7150822d57160acae139cdd2a6373b24eb25b
brunovgois <brunovgois@gmail.com>
2023-07-10 17:40:50 -0300

    save state to localstore and update context

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,9 +1,10 @@
-import { createContext, ReactNode, useReducer } from "react";
+import { createContext, ReactNode, useEffect, useReducer } from "react";
 import { coffeeType } from "../mock/coffe.js";
 import {
   addNewItemAction,
-  changeItemAmount,
+  changeItemAmountAction,
   removeItemAction,
+  clearCartAction
 } from "../reducer/carts/actions.js";
 
 import cartReducer from "../reducer/carts/reducer";
@@ -15,17 +16,40 @@ interface CartItem extends coffeeType {
 type CoffeeCartContextType = {
   cartItems: CartItem[];
   handleAddToCart: (cofee: CartItem) => void;
+  handleClearCart: () => void;
   handleRemoveFromCart: (index: number) => void;
   handleChangeItemAmountFromCart: (
     cartItemIndex: number,
     amount: number
   ) => void;
+  totalCartPrice: number;
 };
 
 export const CoffeeCartContext = createContext({} as CoffeeCartContextType);
 
 export function CoffeeCartProvider({ children }: { children: ReactNode }) {
-  const [state, dispatch] = useReducer(cartReducer, []);
+  const [state, dispatch] = useReducer(cartReducer, [], (initialState) => {
+    const storedStateAsJson = localStorage.getItem(
+      "@coffee-marketplace:cart-state"
+    );
+    if (storedStateAsJson) {
+      return JSON.parse(storedStateAsJson);
+    }
+    return initialState;
+  });
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(state);
+
+    localStorage.setItem("@coffee-marketplace:cart-state", stateJSON);
+  }, [state]);
+  const totalCartPrice = parseInt(
+    state
+      .reduce((prev, cartItem) => {
+        return prev + cartItem.price * cartItem.quantity;
+      }, 0)
+      .toFixed(2)
+  );
 
   function handleAddToCart(newCartItem: CartItem) {
     dispatch(addNewItemAction(newCartItem));
@@ -39,14 +63,20 @@ export function CoffeeCartProvider({ children }: { children: ReactNode }) {
     cartItemIndex: number,
     amount: number
   ) {
-    dispatch(changeItemAmount(cartItemIndex, amount));
+    dispatch(changeItemAmountAction(cartItemIndex, amount));
+  }
+
+  function handleClearCart(){
+    dispatch(clearCartAction())
   }
 
   const value = {
     cartItems: state,
+    totalCartPrice,
     handleAddToCart,
     handleRemoveFromCart,
     handleChangeItemAmountFromCart,
+    handleClearCart
   };
 
   return (
